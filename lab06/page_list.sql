c5ea045aca4e35d6d0959f25e9922ae213b23e37
Kamil0171 <115219623+Kamil0171@users.noreply.github.com>
2024-12-29 14:08:05 +0100

    Add files via upload

--- lab06/page_list.sql
+++ lab06/page_list.sql
@@ -1,3 +1,7 @@
+CREATE DATABASE IF NOT EXISTS moja_strona;
+
+USE moja_strona;
+
 CREATE TABLE IF NOT EXISTS page_list (
     id INT AUTO_INCREMENT PRIMARY KEY,
     page_title VARCHAR(255) NOT NULL,
